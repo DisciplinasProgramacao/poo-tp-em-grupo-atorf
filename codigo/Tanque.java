9c03966d84c2621a67869e0e95c208947a3d92c4
Rodrigo Tadeu <rodrigotadeu61@gmail.com>
2023-12-11 21:12:56 -0300

    atualização

--- codigo/Tanque.java
+++ codigo/Tanque.java
@@ -23,24 +23,20 @@ public class Tanque {
 
 	public double abastecer(double litros) {
 		if (capacidadeAtual + litros <= capacidadeMaxima) {
-			System.out.println("Carro abastecido.");
 			double totalAbastecido = capacidadeAtual + litros;
 			return totalAbastecido;
 		} else {
-			System.out.println("Carro não pode ser abastecido com essa quantidade de litros.");
 			return capacidadeAtual;
 		}
 	}
 
 	public double autonomiaMaxima() {
 		double autonomiaMaxima=capacidadeMaxima/CONSUMO;
-		System.out.println("Aautonomia máxima do carro é de: " + autonomiaMaxima);
 		return autonomiaMaxima;
 	}
 
 	public double autonomiaAtual() {
 		double autonomiaAtual=capacidadeAtual/CONSUMO;
-		System.out.println("Aautonomia máxima do carro é de: " + autonomiaAtual);
 		return autonomiaAtual;
 	}
 
